00104e4015cd54fd6314db5a4630206ecbe2af85
bertoSC <a23albertogc@iessanclemente.net>
2024-11-18 14:32:56 +0100

    commit deserializers

--- src/main/java/org/example/CodigoPostalDeserializer.java
+++ src/main/java/org/example/CodigoPostalDeserializer.java
@@ -4,21 +4,28 @@ import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CodigoPostalDeserializer implements JsonDeserializer <CodigoPostal>{
-    Type tipo = new TypeToken<List<Lugar>>() {}.getType();
+    //Type tipo = new TypeToken<List<Lugar>>() {}.getType();
     @Override
     public CodigoPostal deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
         JsonObject jo = jsonElement.getAsJsonObject();
         String codigoPostal = jo.get("post code").getAsString();
         String pais = jo.get("country").getAsString();
         String abreviatura = jo.get("country abbreviation").getAsString();
-        List<Lugar> lugares = jsonDeserializationContext.deserialize(jsonElement, tipo);
+
+        JsonArray ja = jo.getAsJsonArray("places");
+        List<Lugar> lugares = new ArrayList<>();
+        for (JsonElement je: ja) {
+            Lugar lug = jsonDeserializationContext.deserialize(je, Lugar.class);
+            lugares.add(lug);
+        }
 
         CodigoPostal cp = new CodigoPostal(codigoPostal, pais, abreviatura, lugares);
 
 
-        return null;
+        return cp;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,88 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        //Type listProvType = new TypeToken<List<Lugar>>() {}.getType();
+
+        System.out.println("INFORMACIÓN DE CÓDIGOS POSTALES");
+        System.out.println();
+        System.out.println("Introduce el Código Postal que quieres consultar");
+
+        Scanner sc = new Scanner(System.in);
+        //int idConcello = 15078; // ID de ejemplo para Santiago de Compostela
+        int idCP = sc.nextInt();
+        String json = null;
+        //Boolean existe = comprobarIdConcello(idConcello);
+
+       // if (existe) {
+            System.out.println();
+            System.out.println("INFORMACIÓN DE CP:");
+            System.out.println();
+            json = obtenerCodigoPostalJson(idCP);
+       // }
+
+        if (json != null) {
+            Gson gson = construirGsonBuilder();
+            CodigoPostal p = gson.fromJson(json, CodigoPostal.class);
+            System.out.println(p);
+
+        } else {
+            System.out.println("No se pudo obtener la predicción.");
+        }
+
     }
 
+    public static String obtenerCodigoPostalJson(int idCodigo) {
+        String urlAcceso = "https://api.zippopotam.us/es/" + idCodigo;
+        try {
+            URI uri = new URI(urlAcceso);
+            HttpURLConnection conexion = (HttpURLConnection) uri.toURL().openConnection();
+            conexion.setRequestMethod("GET");
+            if (conexion.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                try (var br = new BufferedReader(new InputStreamReader(conexion.getInputStream()))) {
+                    String linea = null;
+                    StringBuilder codigoPostalJson = new StringBuilder();
+                    while ((linea = br.readLine()) != null) {
+                        codigoPostalJson.append(linea);
+                    }
+                    return codigoPostalJson.toString();
+                }
+            } else {
+                System.out.println("Error en la petición: " + conexion.getResponseCode());
+                return null;
+            }
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Gson construirGsonBuilder(){
+        Type listType = new TypeToken<List<Lugar>>() {}.getType();
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .registerTypeAdapter(CodigoPostal.class, new CodigoPostalDeserializer())
+                .registerTypeAdapter(Lugar.class, new LugarDeserializer())
+                .create();
+        return gson;
+    }
 }
